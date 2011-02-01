d06a664b4de42081d0e10c00c2e5a5da6f4a0fe9
Adam Christian <adam.christian@gmail.com>
2011-02-01 00:36:00 -0800

    Adding new test org

---
+++ test/test.chrome.js
@@ -0,0 +1,32 @@
+var assert = require('assert')
+  , jellyfish = require('../lib/main');
+
+var chrome = jellyfish.createChrome();
+
+chrome.on('command', function(cmd, args){
+  console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args);
+});
+// chrome.on('output', function(cmd, args){
+//   console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args);
+// });
+
+chrome.go("http://www.google.com")
+  .js("document.title", function(o) {
+    assert.equal(o.result,"Google")
+  })
+  .user("type", { query:'input[name="q"]', text:'moo'}, function(o) {
+    console.log(o.result);
+  })
+  .js("$jfQ('input[name=\"q\"]')[0].value", function(o) {
+    console.log(o.result);
+  })
+  .user("click", { query:'input[name="btnG"]' }, function(o) {
+    console.log(o.result);
+  })
+  .jsfile("./test.js", function(o) {
+    console.log(o.result)
+  })
+  .jsurl("http://jelly.io/test.js", function(o) { 
+    console.log(o);
+    process.exit();
+  })
