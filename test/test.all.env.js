aeabd1d92b0cf444aae09847fbef83b82283eb7f
Adam Christian <adam.christian@gmail.com>
2011-04-28 02:59:35 -0700

    More tweaks

--- test/test.all.env.js
+++ test/test.all.env.js
@@ -6,22 +6,22 @@ var done = [];
 var test = function(b) {
   b.go("http://www.wikipedia.com")
     .js("document.title", function(o) {
-      console.log(b.name + ": " + JSON.stringify(o));
+      console.log(b.name +" - "+b.tid+": " + JSON.stringify(o));
       assert.equal(o.result,"Wikipedia")
     })
     .js("document.getElementById(\'searchInput\').value = \'test\'", function(o) {
-      console.log(b.name + ": " + JSON.stringify(o));
+      console.log(b.name +" - "+b.tid+": " + JSON.stringify(o));
     })
     .js("document.getElementById(\'searchInput\').value", function(o) {
-      console.log(b.name + ": " + JSON.stringify(o));
+      console.log(b.name +" - "+b.tid+": " + JSON.stringify(o));
     })
     .js("document.getElementsByName(\'go\')[0].click()", function(o) {
-      console.log(b.name + ": click");
+      console.log(b.name+" - "+b.tid +": click");
     })
     .jsfile("./test.js", function(o) {
-      console.log(b.name + ": " + JSON.stringify(o));
+      console.log(b.name +" - "+b.tid+": " + JSON.stringify(o));
       b.stop(function() {
-        console.log(b.name+": DONE");
+        console.log(b.name +" - "+b.tid+": DONE");
       });
     })
 };
@@ -30,7 +30,7 @@ var browsers = [];
 browsers.push(jellyfish.createFirefox());
 browsers.push(jellyfish.createChrome());
 browsers.push(jellyfish.createSafari());
-browsers.push(jellyfish.createSauce());
+//browsers.push(jellyfish.createSauce());
 //browsers.push(jellyfish.createZombie());
 
 browsers.forEach(function(o) {
